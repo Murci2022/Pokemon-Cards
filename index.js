ab8a32f6f767f01f9e05f95b0729a19d4ade8bed
Ömer Algin <oemer-algin@hotmail.de>
2022-08-20 00:29:43 +0200

    pokemon api

--- index.js
+++ index.js
@@ -1,68 +1,31 @@
-/* console.clear();
+const apiPokemonUrl = 'https://pokeapi.co/api/v2/pokemon/'
 
-console.log('It works!');
-
-const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto';
-
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => createPokemonList(data.result));
-
-function createPokemonList(pokemons) {
-  const list = document.createElement('ul');
-  list.className = 'pokemon__list';
-  document.body.append(list);
-
-  pokemons.forEach(pokemon => {
-    const item = document.createElement('li');
-    item.className = 'pokemon__list-infos';
-
-    item.textContent = pokemon.name;
-    item.append(item);
-  });
-} */
-
-console.clear();
-
-console.log('It works!');
-
-const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto';
-
-fetchPokemon(apiUrl);
-
-async function fetchPokemon() {
-  try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-    const data = await response.json();
-    createPokemonList(data);
-  } catch (error) {
-    console.log(error.message);
-  }
+async function fetchPokemons() {
+    try {
+        const response = await fetch(apiPokemonUrl);
+        const data = await response.json();
+        const pokemons = data.results;
+        createPokemonsList(pokemons);
+    }  catch (error) {
+        console.log(error.message);
+    }
 }
-fetchPokemon();
 
-/* fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => createPokemonList(data.result));
- */
-function createPokemonList(pokemons) {
-  const list = document.createElement('ul');
-  list.className = 'pokemon__list';
-  document.body.append(list);
+fetchPokemons()
 
-  pokemons.forEach(pokemon => {
-    const item = document.createElement('li');
-    item.className = 'pokemon__list-infos';
+function createPokemonsList(pokemons) {
+    const list = document.createElement('ul');
+    list.className = 'pokemon__list'
+    document.body.append(list);
 
-    item.innerHTML = `
-     /* <h2></h2>  */
-     
-     <ul>
-     <li>$[{ability.name}] </li>
-     
-   </ul>
-     
-     `;
-    list.append(item);
-  });
+    pokemons.forEach(pokemon => {
+        const item = document.createElement('li');
+        item.classList = 'pokemon__list--infos';
+
+        item.innerHTML = `${pokemon.name}`;
+
+        list.append(item);
+    })
 }
+
+
