61a02ebe704de2d80d15bae34850f56294aac378
Mate Kamaras <103876505+Murci2022@users.noreply.github.com>
2022-08-19 15:36:44 +0200

    add fetch in index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+console.clear();
 
+console.log('It works!');
+
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => createPokemonList(data.result));
+
+function createPokemonList(pokemons) {
+  const list = document.createElement('ul');
+  list.className = 'pokemon__list';
+  document.body.append(list);
+
+  pokemons.forEach(pokemon => {
+    const item = document.createElement('li');
+    item.className = 'pokemon__list-infos';
+
+    item.textContent = pokemon.name;
+    item.append(item);
+  });
+}
