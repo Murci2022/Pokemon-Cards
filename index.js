14d44d080e402f7de5396a190be7f9bd26c3306b
Mate Kamaras <mate.kamaras@icloud.com>
2022-08-21 12:23:08 +0200

    add new js code and new version of card.css

--- index.js
+++ index.js
@@ -1,44 +1,70 @@
 
-const apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+const pokedex = document.getElementById('pokedex');
+const pokemon_count =150;
 
 
 
-async function fetchPokemon() {
+
+async function fetchPokemons() {
   try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data.results)
+    for (let i = 1; i <= pokemon_count; i++) {
+      await getPokemon(i);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-    createPokemonList(data.results);
+fetchPokemons();
+
+async function getPokemon(id)
+ {
+  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  console.log(data);
+  createPokedexCard(data);
+}
+   /*  createPokemonList(data.results);
     console.log(data)
   } catch (error) {
     console.log(error.message);
   }
 }
-fetchPokemon(apiUrl);
-
+fetchPokemon(apiUrl); */
 /* fetch(apiUrl)
   .then(response => response.json())
   .then(data => createPokemonList(data.result));
  */
-function createPokemonList(pokemons) {
-  const list = document.createElement('ul');
-  list.className = 'list';
-  document.body.append(list);
-
-  pokemons.forEach(pokemon => {
-    const item = document.createElement('li');
-    item.className = 'item';
-
-    item.innerHTML = `
-     <h2>${pokemon.name}</h2> 
-     
-     
+function createPokedexCard(data) {
+  const imageUrl = data.sprites.other["official-artwork"].front_default;
+  
+  const card = document.createElement("div");
+  card.className = "pokedex__card";
+  card.innerHTML = `
+  
+  <span class="pokedex__card-id">No.${data.id}</span>
+  <img class="pokedex__card-img" src=${imageUrl} alt="image"/>
+  <button class="icon-card">
+  
+  <svg
+    class="card--icon_down"
     
-     
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12M6,10L12,16L18,10L16.6,8.6L12,13.2L7.4,8.6L6,10Z"
+    />
+  </svg>
+</button>
+  <h2 class="pokedex__card-name">${data.name.toUpperCase()}</h2>
+  <span class="type">Type: ${data.types[0].type.name}</span>
+  <span class="type">Weight: ${data.weight}</span>
+  <span class="type">Height: ${data.height}</span>
+
+  
      `;
-    list.append(item);
-  });
+  pokedex.append(card);
 }
 
-fetchPokemon();
+
